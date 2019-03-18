ce786491c1a6aad4ea7f8c79ab9e4262e5335682
Thespartann <danilastefan69@yahoo.ro>
2019-03-18 18:10:46 +0000

    KaliRom: Add Wi-Fi firmware required for a few external wireless adapters
    * To be used with Nethunter
    Thanks to rithvikvibhu, files taken from https://github.com/rithvikvibhu/nh-magisk-wifi-firmware
    
    Signed-off-by: Thespartann <danilastefan69@yahoo.ro>

--- config/kali.mk
+++ config/kali.mk
@@ -3,3 +3,6 @@ PRODUCT_PACKAGES += \
     Nethunter \
     Nethunter-VNC \
     Nethunter-Terminal
+
+# Copy Wi-Fi firmware
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/cm/prebuilt/common/etc/firmware,system/etc/firmware)
